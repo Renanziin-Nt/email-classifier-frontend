f0a57949174e6873dfe441d93916048f545557c2
Renan Dos Santos <renan.santosoliveira0@gmail.com>
2025-09-16 17:32:52 -0300

    feat: update env script

--- scripts/env.js
+++ scripts/env.js
@@ -1,6 +1,6 @@
 const env = {
-  BACKEND_WS: window.__env?.BACKEND_WS || 'localhost:8000',
-  BACKEND_API: window.__env?.BACKEND_API || 'http://localhost:8000',
+  BACKEND_WS: window.__env?.BACKEND_WS,
+  BACKEND_API: window.__env?.BACKEND_API,
   WS_PROTOCOL: window.__env?.WS_PROTOCOL || 'ws',
   SECURE: window.__env?.SECURE || false
 };
